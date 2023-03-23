68111c498c33d3bc7865478411ee15a867784cd9
Pabs <paul.mca.ca@gmail.com>
2023-03-23 17:24:56 +0000

    Adding excerises for whichSchool, loopyLighthouse and hello

---
+++ whichSchool.js
@@ -0,0 +1,9 @@
+const whichSchool  = function (age) {
+  if (age < 13){
+    console.log("You should go to Elementary school.")
+  } else if (age >= 13 && age <= 18){
+    console.log("You should go to Secondary School.")
+  } else {
+    console.log("You should go to Lighthouse Labs")
+  }
+}
